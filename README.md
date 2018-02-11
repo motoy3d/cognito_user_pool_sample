572318454ebfc92846d38987a06cf8c4116e858e
EC2 Default User <ec2-user@ip-172-31-8-179.ap-northeast-1.compute.internal>
2018-02-11 12:17:29 +0900

    細かい部分をカスタマイズ。

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# cognito_user_pool_sample
+# cognito_userpools_sample
 
-ブログ記事『Cognito ユーザープール使ってみました』で使ったサンプルコード
+Amazon Cognito User Poolsを使用した認証サービスサンプル
 
+- signup.html
+- login.html
+- mypage.html
+- confirm.html
+- resend.html
+
+フォーク元
 http://blog.mori-soft.com/entry/2017/09/06/160626
 
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict'
-var upsample = {};
+var userPools = {};
 
-upsample.poolData = {
-    UserPoolId: 'ユーザープールID',
-    ClientId: 'アプリクライアントID'
+// 各AWSアカウントのUserPools固有情報
+userPools.poolData = {
+    UserPoolId: 'ap-northeast-1_8g798FSlW',
+    ClientId: '22do653qgnj9827pdt1rg9hdp1'
 };
-upsample.UserPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(upsample.poolData);
+userPools.UserPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(userPools.poolData);
 
-upsample.signup = function() {
+/**
+ * 入力されたメールアドレス、ユーザー名、パスワードでサインアップする。
+ */
+userPools.signup = function() {
     var email = $('#inputEmail').val();
     var username = $('#inputUserName').val();
     var password = $('#inputPassword').val();
@@ -18,7 +22,7 @@ upsample.signup = function() {
     attributeList.push(attributeEmail);
 
     var message_text;
-    upsample.UserPool.signUp(username, password, attributeList, null, function(err, result){
+    userPools.UserPool.signUp(username, password, attributeList, null, function(err, result){
         if (err) {
             console.log(err);
             message_text = err;
@@ -33,14 +37,17 @@ upsample.signup = function() {
     });
 }
 
-upsample.verify = function() {
+/**
+ * 入力された確認コードを検証して結果を表示する。
+ */
+userPools.verify = function() {
     var username = $('#inputUserName').val();
     var vericode = $('#inputVerificationCode').val();
     if (!username | !vericode) { return false; }
 
     var userData = {
         Username: username,
-        Pool: upsample.UserPool
+        Pool: userPools.UserPool
     };
 
     var message_text;
@@ -61,14 +68,16 @@ upsample.verify = function() {
     });
 }
 
-
-upsample.resend = function() {
+/**
+ * 確認コードを再送する。
+ */
+userPools.resend = function() {
     var username = $('#inputUserName').val();
     if (!username) { return false; }
 
     var userData = {
         Username: username,
-        Pool: upsample.UserPool
+        Pool: userPools.UserPool
     };
 
     var message_text;
@@ -87,7 +96,10 @@ upsample.resend = function() {
     });
 }
 
-upsample.login = function() {
+/**
+ * 入力されたユーザー名・パスワードでログイン処理を行う。
+ */
+userPools.login = function() {
     var username = $('#inputUserName').val();
     var password = $('#inputPassword').val();
     if (!username | !password) { return false; }
@@ -100,7 +112,7 @@ upsample.login = function() {
 
     var userData = {
         Username: username,
-        Pool: upsample.UserPool
+        Pool: userPools.UserPool
     };
 
     var message_text;
@@ -131,17 +143,83 @@ upsample.login = function() {
             
             //$(location).attr('href', 'mypage.html');
         },
+        mfaRequired: function(codeDeliveryDetails) {
+            // MFA is required to complete user authentication.
+            // Get the code from user and call
+//            cognitoUser.sendMFACode(mfaCode, this)
+            console.log("mfaCode要確認");
+        },
+        // 新パスワードの設定が必要な場合
+        newPasswordRequired: function(userAttributes, requiredAttributes) {
+            // User was signed up by an admin and must provide new
+            // password and required attributes, if any, to complete
+            // authentication.
 
+            // the api doesn't accept this field back
+            delete userAttributes.email_verified;
+
+            // Get these details and call
+            var newPassword = prompt('新しいパスワードを入力してください: ' ,'');
+            cognitoUser.completeNewPasswordChallenge(newPassword, userAttributes, this);
+        },
         onFailure: function(err) {
-            alert(err);
+            if (err && err.code == 'PasswordResetRequiredException') {
+                var verificationCode = prompt('検証コードを入力してください ' ,'');
+                var newPassword = prompt('新しいパスワードを入力してください ' ,'');
+                cognitoUser.confirmPassword(verificationCode, newPassword, {
+                    onSuccess() {
+                        console.log('Password confirmed!');
+                    },
+                    onFailure(err) {
+                        console.log('Password not confirmed!');
+                    }
+                });
+                
+                if (true) {
+                    return;
+                    //↓はユーザーが「パスワードを忘れた」をクリックした時に呼び出すべきメソッド。
+                    // だと思う。
+                }
+                // パスワードリセットが要求されているステータスの場合の処理
+                cognitoUser.forgotPassword({
+                    onSuccess: function (data) {
+                        // successfully initiated reset password request
+            	          console.log('CodeDeliveryData from forgotPassword: ' + data);
+                    },
+                    onFailure: function(err) {
+                        alert(err);
+                    },
+                    //確認コードがメールで来るので、↓のプロンプトで入力する。
+                    //新パスワードも入力して再設定する。 
+                    inputVerificationCode: function(data) {
+                        console.log('Code sent to: ' + data);
+                        var verificationCode = prompt('検証コードを入力してください ' ,'');
+                        var newPassword = prompt('新しいパスワードを入力してください ' ,'');
+                        cognitoUser.confirmPassword(verificationCode, newPassword, {
+                            onSuccess() {
+                                console.log('Password confirmed!');
+                            },
+                            onFailure(err) {
+                                console.log('Password not confirmed!');
+                            }
+                        });
+                    }
+                });
+            } else {
+                alert(err);
+            }
         }
     });
 
 }
 
-upsample.checkSession = function () {
+/**
+ * ログイン状態をチェックしてログイン済みならユーザー情報を取得して画面に表示。
+ * ログアウト状態ならログイン画面へ遷移。
+ */
+userPools.checkSession = function () {
 
-    var cognitoUser = upsample.UserPool.getCurrentUser();
+    var cognitoUser = userPools.UserPool.getCurrentUser();
     if (cognitoUser != null) {
         cognitoUser.getSession(function (err, sessionResult) {
             if (sessionResult) {
@@ -172,12 +250,13 @@ upsample.checkSession = function () {
     }
 }
 
-upsample.logout = function() {
-
-    var cognitoUser = upsample.UserPool.getCurrentUser();
+/**
+ * ログアウトする。
+ */
+userPools.logout = function() {
+    var cognitoUser = userPools.UserPool.getCurrentUser();
     if (cognitoUser != null) {
         cognitoUser.signOut();
         location.reload();
     }
-
 }
